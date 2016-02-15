433e589c924b81f088be1c7a6655dde83c1594ec
James510 <jamesl510@yahoo.com>
2016-02-14 21:36:19 -0600

    Basic Engine

---
+++ Assets/_Scripts/CameraOperator.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraOperator : MonoBehaviour
+{
+    public Texture2D selectionHighlight = null;
+    public static Rect selection = new Rect(0, 0, 0, 0);
+    private Vector3 startClick = -Vector3.one;
+    private static Vector3 moveToDestination = Vector3.zero;
+    private static List<string> passables = new List<string>() { "Floor" };
+	// Update is called once per frame
+	void Update ()
+    {
+        CheckCamera();
+        Cleanup();
+	}
+    void CheckCamera()
+    {
+        if (Input.GetMouseButtonDown(0))
+            startClick = Input.mousePosition;
+        else if(Input.GetMouseButtonUp(0)) 
+            startClick = -Vector3.one;
+
+        if (Input.GetMouseButton(0))
+        {
+
+            if (selection.width < 0)
+            {
+                selection.x += selection.width;
+                selection.width = selection.width;
+            }
+            if (selection.height < 0)
+            {
+                selection.y += selection.height;
+                selection.height = selection.height;
+            }
+            selection = new Rect(startClick.x, InvertMouseY(startClick.y), Input.mousePosition.x - startClick.x, InvertMouseY(Input.mousePosition.y) - InvertMouseY(startClick.y));
+
+        }
+    }
+    public static float InvertMouseY(float y)
+    {
+        return Screen.height - y;
+    }
+    void OnGUI()
+    {
+        if(startClick != -Vector3.one)
+        {
+            GUI.color = new Color(1, 1, 1, 0.5f);
+            GUI.DrawTexture(selection, selectionHighlight);
+        }
+    }
+    void Cleanup()
+    {
+        if (!Input.GetMouseButtonUp(1))
+            moveToDestination = Vector3.zero;
+    }
+
+    public static Vector3 GetDestination()
+    {
+        if(moveToDestination == Vector3.zero)
+        {
+            RaycastHit hit;
+            Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(r,out hit))
+            {
+                while(!passables.Contains(hit.transform.gameObject.name))
+                {
+                    if (!Physics.Raycast(hit.point + r.direction * 0.1f,r.direction,out hit))
+                        break;
+                }
+            }
+            if (hit.transform != null)
+                moveToDestination = hit.point;
+        }
+        return moveToDestination;
+    }
+
+    
+}
---
+++ Assets/_Scripts/Unit.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour
+{
+    public bool selected = false;
+    public float floorOffset = 1;
+    public float speed = 5.0f;
+    public float stopDistanceOffset = 1;
+    public float rotSpeed;
+    private Vector3 moveToDest = Vector3.zero;
+    private Rigidbody rb;
+    private bool selectedByClick=false;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(GetComponent<Renderer>().isVisible&&Input.GetMouseButton(0))
+        {
+            if(!selectedByClick)
+            {
+                Vector3 camPos = Camera.main.WorldToScreenPoint(transform.position);
+                camPos.y = CameraOperator.InvertMouseY(camPos.y);
+                selected = CameraOperator.selection.Contains(camPos);
+            }
+            if (selected)
+                GetComponent<Renderer>().material.color = Color.red;
+            else
+                GetComponent<Renderer>().material.color = Color.white;
+        }
+        if (selected && Input.GetMouseButtonUp(1))
+        {
+            Vector3 destination = CameraOperator.GetDestination();
+
+            if(destination != Vector3.zero)
+            {
+                //gameObject.GetComponent<NavMeshAgent>().SetDestination(destination); //Unity Pro
+                moveToDest = destination;
+                moveToDest.y += floorOffset;
+            }
+        }
+
+        UpdateMove();
+    }
+    void UpdateMove()
+    {
+        if(moveToDest != Vector3.zero && transform.position != moveToDest)
+        {
+            /*Vector3 direction = (moveToDest - transform.position).normalized;
+            direction.y = 0;
+            rb.velocity = direction * speed;*/
+
+            Vector3 toTarget = moveToDest - transform.position;
+            float step = rotSpeed * Time.deltaTime;
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, toTarget, step, 0.0f);
+            transform.rotation = Quaternion.LookRotation(newDir);
+            float dist = Vector3.Distance(moveToDest, transform.position);
+            rb.velocity = transform.forward * speed;
+
+            if (Vector3.Distance(transform.position, moveToDest) < stopDistanceOffset)
+                moveToDest = Vector3.zero;
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        selectedByClick = true;
+        selected = true;
+    }
+    void OnMouseUp()
+    {
+        if (selectedByClick)
+            selected = true;
+        selectedByClick = false;
+    }
+}
