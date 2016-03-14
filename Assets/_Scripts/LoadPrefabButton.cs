e9ee6f2c6c5fbf7acc9f0709fd9d3b6150bc1899
James510 <jamesl510@yahoo.com>
2016-03-13 19:01:48 -0500

    Shipbuilder + Transforms

---
+++ Assets/_Scripts/LoadPrefabButton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoadPrefabButton : MonoBehaviour
+{
+    public int  prefabNum=0;
+    private GameObject shipBuilderCore;
+    private Button loadButton;
+
+<<<<<<< HEAD
+=======
+    // Use this for initialization
+>>>>>>> origin/master
+    void Start()
+    {
+        shipBuilderCore = GameObject.Find("ShipBuilderCore");
+        Text shipText = transform.GetChild(0).GetComponent<Text>();
+        shipText.text = this.name;
+    }
+
+<<<<<<< HEAD
+=======
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+>>>>>>> origin/master
+    void Awake()
+    {
+        loadButton = GetComponent<Button>();
+        SetAction();
+    }
+
+    public void SetAction()
+    {
+        loadButton.onClick.RemoveAllListeners();
+        loadButton.onClick.AddListener(() => { shipBuilderCore.SendMessage("LoadPrefab", prefabNum); });
+    }
+}
--- Assets/_Scripts/MainCameraMover.cs
+++ Assets/_Scripts/MainCameraMover.cs
@@ -7,17 +7,19 @@ public class MainCameraMover : MonoBehaviour
     public float rotateSpeed = 50;
     public float ScrollEdge = 0.1f;
     public float PanSpeed = 10;
-    private Vector3 initialPosition;
+    public float moveSpeed = 10;
+
+
 	void Update ()
     {
         if (Input.GetKey("d") && !Input.GetKey("a"))
-            transform.Translate(Vector3.right * Time.deltaTime * PanSpeed, Space.Self);
+            transform.Translate(transform.right * Time.deltaTime * PanSpeed, Space.World);
         else if (Input.GetKey("a") && !Input.GetKey("d"))
-            transform.Translate(Vector3.right * Time.deltaTime * -PanSpeed, Space.Self);
+            transform.Translate(transform.right * Time.deltaTime * -PanSpeed, Space.World);
         if (Input.GetKey("w") && !Input.GetKey("s"))// || Input.mousePosition.y >= Screen.height * (1 - ScrollEdge) //Scroll when at edge
-            transform.Translate(Vector3.forward * Time.deltaTime * PanSpeed, Space.Self);
+            transform.Translate(transform.forward * Time.deltaTime * PanSpeed, Space.World);
         else if (Input.GetKey("s") && !Input.GetKey("w"))// || Input.mousePosition.y <= Screen.height * ScrollEdge
-            transform.Translate(Vector3.forward * Time.deltaTime * -PanSpeed, Space.Self);
+            transform.Translate(transform.forward * Time.deltaTime * -PanSpeed, Space.World);
 
         if (Input.GetKey("q")) // || Input.mousePosition.x <= Screen.width * ScrollEdge
         {
@@ -26,6 +28,15 @@ public class MainCameraMover : MonoBehaviour
         else if (Input.GetKey("e"))// || Input.mousePosition.x >= Screen.width * (1 - ScrollEdge)
         {
             transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed, Space.World);
+        }
+        /*if (Input.GetMouseButton(1))
+        {
+            transform.Translate(transform.right * -Input.GetAxis("Mouse X") * moveSpeed, Space.World);
+            transform.Translate(transform.forward * -Input.GetAxis("Mouse Y") * moveSpeed, Space.World);
+        }*/
+        if(Input.GetMouseButtonDown(2))
+        {
+            transform.rotation = Quaternion.Euler(0.0f, transform.GetChild(0).eulerAngles.y, 0.0f);
         }
     }
 }
